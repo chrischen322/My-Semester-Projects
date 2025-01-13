649b7dc99dffa6be3b759567444f46850790f155
chrischen322 <cchen32@cps.edu>
2025-01-13 08:33:39 -0600

    Update SimpleCalc.py
    This is a simple calculator function that has the ability to perform 4 operations: addition, subtraction, multiplication and division.

--- SimpleCalc.py
+++ SimpleCalc.py
@@ -7,13 +7,13 @@
 def calculator():
     print("Welcome to Simple Calculator.")
     while True:
-        print("Please choose an operation.")
+        print("Please choose an operation.")  #1-5 asks the user what function they want to perform. 1 = Add, 2 = Subtract, 3 = Multiply, 4 = Divide, 5 = Quit
         print("""1. Add
         2. Subtract
         3. Multiply
         4. Divide
         5. Quit """)
-        option = int(input("1-5:"))
+        option = int(input("1-5:"))  #Asking the user for the respective numbers that will be used when performing the selected operation
         if option == 1:
             num1 = int(input("Please enter the first number: "))
             num2 = int(input("Please enter the second number: "))
@@ -30,10 +30,10 @@ def calculator():
             num1 = int(input("Please enter the first number: "))
             num2 = int(input("Please enter the second number: "))
             divide(num1,num2)
-        if option == 5:
+        if option == 5:   #Quit function if user selects option 5
             quit()
             break
-def add(num1, num2):
+def add(num1, num2):  #Performing indicated functions
     answer = num1 + num2
     print(answer)
 
