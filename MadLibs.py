c98e3591b18cd3ad025c19d1183ab5e92e25a53e
chrischen322 <cchen32@cps.edu>
2025-01-13 08:37:13 -0600

    Update MadLibs.py
    This program asks the user a series of questions and then compiles the answers to a sentence.

--- MadLibs.py
+++ MadLibs.py
@@ -3,10 +3,10 @@
 
 #Init
 def mad_libs():
-    adjective = input("Please enter an adjective: ")
+    adjective = input("Please enter an adjective: ")        #Asks for inputs
     article_of_clothing = input("Please choose an article of clothing: ")
     location = input("Please enter a location: ")
-    food = input("Please choose your favorite food: ")
+    food = input("Please choose your favorite food: ")        #Puts inputs together into the preselected format below
     print("What a" + " " + adjective + " " + "day! I believe it is a great day to wear my" + " " + article_of_clothing + " " + "today. With this weather I must go and visit" + " " + location + "." + " " + "I'd best grab a snack before heading out today. I'm feeling some" + " " + food + ".")
 
 #Function
