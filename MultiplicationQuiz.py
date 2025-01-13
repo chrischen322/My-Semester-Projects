2f610be43143e63307f02bed199a232b427cca37
chrischen322 <cchen32@cps.edu>
2025-01-13 08:35:39 -0600

    Update MultiplicationQuiz.py
    This is a fun multiplication program that allows the user to pick the amount of questions they would like to answer. Each correct answer is rewarded with a point.

--- MultiplicationQuiz.py
+++ MultiplicationQuiz.py
@@ -6,22 +6,22 @@ import random
 global score
 #Function
 def quiz():
-    while True:
+    while True:   #User starts with score of 0
         score = 0
-        questions = input("How many questions would you like to answer?")
+        questions = input("How many questions would you like to answer?")   #Asking how many questions the user wants to answer
         for i in range(int(questions)):
-            x = random.randint(1,10)
+            x = random.randint(1,10)   #Picks random numbers to multiply from 1-10
             y = random.randint(1,10)
-            answer = input("What is " + str(x) + " times " + str(y) + "?")
+            answer = input("What is " + str(x) + " times " + str(y) + "?") #Asks question
             z = (x*y)
             if (int(answer)) == z:
                 print("The answer is correct.")
             else:
                 print("The answer is incorrect.")
-            if (int(answer)) == z:
+            if (int(answer)) == z: #If the answer is correct, user gets +1 point
                 score = (score + 1)
                 print("Your score is " + str(score))
-        playagain = input(("Would you like to play again? YES/NO"))
+        playagain = input(("Would you like to play again? YES/NO")) #Function to loop or break
         if playagain == ("NO"):
             break
 
